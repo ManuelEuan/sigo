6aeb56ae1737bedf4c0fb9cc8022ab570c838b07
AdrianTM_43 <saul.tun@axonsolutions.com.mx>
2022-07-12 08:24:36 -0500

    reportes aglomera

--- application/views/PAT/inicio_PAT.php
+++ application/views/PAT/inicio_PAT.php
@@ -34,7 +34,7 @@
                             {
                                 echo '<div class="col">
                                     <div class="form-group">
-                                        <label class="control-label">Dependencia responsabe</label>
+                                        <label class="control-label">Dependencia responsable</label>
                                         <select type="text" name="search_dependencia" id="search_dependencia" class="form-control" onChange="search(event);" >
                                             <option value="0">--Todos--</option>'.$dependencias.'
                                         </select>
