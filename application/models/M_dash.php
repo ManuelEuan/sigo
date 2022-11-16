2a7044a3b78972c52df9b1e78d2dfdd81a4bf5eb
JonatanSuarez69 <66523762+JonatanSuarez69@users.noreply.github.com>
2022-11-16 13:12:14 -0600

    Colummna que indica el eje rector
    Se agrega colummna que indica en que eje rector se encuentra la accion

--- application/models/M_dash.php
+++ application/models/M_dash.php
@@ -905,16 +905,18 @@ class M_dash extends CI_Model {
 
 	function list_actividades($anio,$dep, $eje) {
 		if($eje != '' || $eje != null || $eje != 0){
-			$sql = 'SELECT act."iIdActividad", act."vActividad", dat."nAvance", dat."iIdDetalleActividad"
+			$sql = 'SELECT act."iIdActividad", act."vActividad", dat."nAvance", dat."iIdDetalleActividad", eje."vEje"
 				FROM "Dependencia" dep
 				INNER JOIN "Actividad" act ON act."iIdDependencia" = dep."iIdDependencia"
+				INNER JOIN "PED2019Eje" eje ON eje."iIdEje" = act."iideje"
 				INNER JOIN "DetalleActividad" dat ON dat."iIdActividad" = act."iIdActividad" AND dat."iActivo" = 1 AND dat."iAnio" = '.$anio.' AND act."iideje" = '.$eje.'
 				WHERE act."iIdDependencia" = '.$dep.'
 				ORDER BY act."vActividad"';
 		}else{
-			$sql = 'SELECT act."iIdActividad", act."vActividad", dat."nAvance", dat."iIdDetalleActividad"
+			$sql = 'SELECT act."iIdActividad", act."vActividad", dat."nAvance", dat."iIdDetalleActividad", eje."vEje"
 			FROM "Dependencia" dep
 			INNER JOIN "Actividad" act ON act."iIdDependencia" = dep."iIdDependencia"
+			INNER JOIN "PED2019Eje" eje ON eje."iIdEje" = act."iideje"
 			INNER JOIN "DetalleActividad" dat ON dat."iIdActividad" = act."iIdActividad" AND dat."iActivo" = 1 AND dat."iAnio" = '.$anio.'
 			WHERE act."iIdDependencia" = '.$dep.'
 			ORDER BY act."vActividad"';
